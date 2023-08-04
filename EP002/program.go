3b720756104cd500a74174437af6d98edf902d3c
Sumet Jutajan <sumet.jutha@gmail.com>
2023-08-04 17:07:44 +0700

    update

---
+++ EP002/program.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// แสดงผลชนิดของข้อมูล
+func main() {
+	name := "John Tik"
+	// age := 36
+	// score := 95.8
+	// isPass := true
+
+	fmt.Printf("My name is %v", name)
+}
