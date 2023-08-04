9c7334ff295747377c7e2da90a572377b695ec5a
Sumet Jutajan <sumet.jutha@gmail.com>
2023-08-04 16:31:06 +0700

    update

---
+++ ep002_comment.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt" //นำเอา Package เข้ามาทำงาน
+/*
+ให้ทำงานในฟังก์ชั่น main
+มีการแสดงผลออกทางจอภาพ
+โดยการแสดงผลจะอาศัยคำสัง fmtprintln
+*/
+
+func main() {
+	fmt.Println("Hello Go Programing")
+	fmt.Println("John Tik")
+	fmt.Println("I Love Arsenal")
+	// fmt.Println("Dev a Dup Dup")
+}
