d2ae77902bf5ebc7dd3b3d7a5123cc2549c9ceb6
Sumet Jutajan <sumet.jutha@gmail.com>
2023-08-04 16:53:10 +0700

    Update program.go

--- program.go
+++ program.go
@@ -7,9 +7,19 @@ import "fmt" //นำเอา Package เข้ามาทำงาน
 โดยการแสดงผลจะอาศัยคำสัง fmtprintln
 */
 
+// การนิยามตัวแปร
+
 func main() {
-	fmt.Println("Hello Go Programing")
-	fmt.Println("John Tik")
-	fmt.Println("I Love Arsenal")
-	// fmt.Println("Dev a Dup Dup")
+	var name string = "John Tik"
+	var age int = 36
+	var score float32 = 95.8
+	var isPass bool = true
+	name_2 := "Sumet Jutajan" // เขียนแบบสั้นแบบนี้ได้
+
+	fmt.Println("คะแนนสอบ => ", score)
+	fmt.Println("My name is", name)
+	fmt.Println("Age is => ", age, "year")
+	fmt.Println("สอบผ่านมั๊ย => ", isPass)
+	fmt.Println("ชื่อจริงๆ => ", name_2)
+	fmt.Println("Hello John Tik")
 }
