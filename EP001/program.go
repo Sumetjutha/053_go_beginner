91e2db353b40982766dceef16d89b10bb9b5864e
Sumet Jutajan <sumet.jutha@gmail.com>
2023-08-04 17:03:17 +0700

    update

--- EP001/program.go
+++ EP001/program.go
@@ -15,11 +15,19 @@ func main() {
 	var score float32 = 95.8
 	var isPass bool = true
 	name_2 := "Sumet Jutajan" // เขียนแบบสั้นแบบนี้ได้
+	name = "Martin Odegaard"  // เปลี่ยนแปลงข้อมูลได้
+
+	/*
+	 ตัวแปร constant คือค่าคงที่ที่ไม่สามารถเปลี่ยนแปลงได้
+	*/
+	// const name_3 string = "Pipupu"
+	// name_3 = "Goky"
 
 	fmt.Println("คะแนนสอบ => ", score)
 	fmt.Println("My name is", name)
 	fmt.Println("Age is => ", age, "year")
 	fmt.Println("สอบผ่านมั๊ย => ", isPass)
 	fmt.Println("ชื่อจริงๆ => ", name_2)
+	// fmt.Println("ชื่อที่สาม => ", name_3)
 	fmt.Println("Hello John Tik")
 }
