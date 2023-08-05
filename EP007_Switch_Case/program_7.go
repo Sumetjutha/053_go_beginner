830260e75a6055a51f8ea20ef5e19f3822a20792
Sumet Jutajan <sumet.jutha@gmail.com>
2023-08-05 16:44:40 +0700

    Create program_7.go

---
+++ EP007_Switch_Case/program_7.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Switch..Case
+func main() {
+	var number int
+	fmt.Print("ป้อนตัวเลข = ")
+	fmt.Scanf("%d", &number)
+	fmt.Println("ตัวเลข = ", number)
+
+	if number == 1 {
+		fmt.Println("เปิดบัญชีใหม่")
+	} else if number == 2 {
+		fmt.Println("ฝากเงิน - ถอนเงิน")
+	} else {
+		fmt.Println("ข้อมูลไม่ถูกต้อง")
+	}
+}
